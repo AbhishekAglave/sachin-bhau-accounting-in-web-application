c98d52e03ec1dcd500c10f428ae8b6f58feafe05
abhishek <abhishekaglave@grampower.com>
2022-10-29 23:23:54 +0530

    just

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const cors = require("cors");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -15,11 +17,19 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/frontend")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
